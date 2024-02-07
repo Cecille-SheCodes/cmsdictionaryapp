39da39003618c832e59df159248df779deebdb62
Cecille-SheCodes <137907754+Cecille-SheCodes@users.noreply.github.com>
2024-02-07 17:14:33 +0100

    initial commit or setup

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+        <h1 className="App-name">
+         My Dictionary App
+        </h1>
+        </header>
+    </div>
+  );
+}
+
+export default App;
