4bb413b59db447015ec35acf94da191033bb169e
Cecille-SheCodes <137907754+Cecille-SheCodes@users.noreply.github.com>
2024-02-09 16:02:28 +0100

    added the dictionary api

--- src/SearchForm.js
+++ src/SearchForm.js
@@ -1,13 +1,20 @@
 import React, {useState} from "react";
 import "./App.css";
+import axios from "axios";
 
 export default function SearchForm () {
 
     let [keyword,setKeyword] = useState("");
 
+    function HandleResponse(response){
+        console.log(response.data[0]);
+    }
+
     function Search(event){
         event.preventDefault(); 
         alert(`Searching for ${keyword} definition`);
+        let apiURL =`https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
+        axios.get(apiURL).then(HandleResponse);
     }
     
     function HandleKeyword(event){
